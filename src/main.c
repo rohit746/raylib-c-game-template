6696bebfb0426f957c00715d3211400f5a3de61d
Rohit Jadhav <45796556+rohit746@users.noreply.github.com>
2021-09-28 21:52:35 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -16,13 +16,13 @@ int main()
         
         ClearBackground(RAYWHITE);
         
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+        const int texture_x = screenWidth / 2 - texture.width / 2;
+        const int texture_y = screenHeight / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
         const char* text = "OMG! IT WORKS!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        DrawText(text, screenWidth / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
         EndDrawing();
     }
