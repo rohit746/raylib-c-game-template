d4e698c49c92e8c0dd01174188c6684d5eecd499
Rohit Jadhav <45796556+rohit746@users.noreply.github.com>
2021-09-28 21:25:00 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,14 +8,26 @@ int main()
     InitWindow(screenWidth, screenHeight, "Main Window");
     SetTargetFPS(60);
 
+    Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
+    
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        
         ClearBackground(RAYWHITE);
-        DrawText("Hello Word!", 10, 10, 20, RED);
+        
+        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+        DrawTexture(texture, texture_x, texture_y, WHITE);
+
+        const char* text = "OMG! IT WORKS!";
+        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+
         EndDrawing();
     }
 
     CloseWindow();
+    
     return 0;
 }
